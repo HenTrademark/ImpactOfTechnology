96409df7ce9e9d5e4d44ae6293757f40ab53908d
HenTrademark <84338282+HenTrademark@users.noreply.github.com>
2023-05-30 01:41:03 +0100

    Finished pretty much everything
    I have tried pushing these files one by one, but something is wrong with my Github. So instead, this is the full version.

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ function w3_close() {
     document.getElementById("myOverlay").style.display = "none";
 }
 
+
+function open_close() {
+    let x = document.getElementById("ethicalIssues");
+    let y = document.getElementById("EI");
+    if (x.className.indexOf("w3-show") === -1) {
+        x.className += " w3-show";
+        y.innerHTML = "<i class=\"fa fa-caret-down\"></i>Ethical issues";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+        y.innerHTML = "<i class=\"fa fa-caret-right\"></i> Ethical issues";
+    }
+}
 function validate() {
 
 }
